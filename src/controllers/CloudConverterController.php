f624ed1a7145d0dd0f97b5c9d1bee2d4e2d56c58
Alex Pavlov <salex.pel@gmail.com>
2018-10-04 09:02:21 +0300

    Added logs

--- src/controllers/CloudConverterController.php
+++ src/controllers/CloudConverterController.php
@@ -19,13 +19,17 @@ class CloudConverterController extends Controller
     {
         $request = $this->getRequest();
         $id = $request->get('id');
+        $fn = fopen('log.txt', 'w+');
+        fputs($fn, '#' . $id . ' step = ' . $request->get('step'));
         if ($id && $request->get('step') == 'finished') {
             $options = Redis::getInstance()->get('cc:' . $id);
+            fputs($fn, $options);
             if ($options) {
                 $options = json_decode($options, true);
                 $api = new Api($options['token']);
                 $process = new Process($api, $request->get('url'));
                 $url = $process->refresh()->output->url;
+                fputs($fn, $url);
                 try {
                     $client = new Client();
                     $client->request('POST', $options['callback'], [
@@ -40,5 +44,6 @@ class CloudConverterController extends Controller
                 }
             }
         }
+        fclose($fn);
     }
 }
