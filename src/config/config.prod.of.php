b7afee4ad1051082c22df492c7722b8f1b5671e1
Alex Pavlov <salex.pel@gmail.com>
2018-11-06 12:41:57 +0300

    Added prod config for onlyfans

---
+++ src/config/config.prod.of.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: pel
+ * Date: 06/11/2018
+ */
+
+return [
+    'baseUrl' => 'https://convert.onlyfans.com',
+    'redis'   => [
+        'host'     => '127.0.0.1',
+        'port'     => 6379,
+        'database' => 1
+    ],
+    'graylog' => [
+        'facility'   => 'prod_of',
+        'connection' => [
+            'port' => 1517,
+            'host' => '163.237.242.4',
+        ]
+    ],
+    'presets' => [
+        'of' => [
+            'video' => [
+                'driver'       => \Converter\components\drivers\CloudConvertDriver::class,
+                'withOutSave'  => true,
+                'token'        => 'TnjiK5KWTh4PU9ceXIiQ9PoRGK_PXZOyR7whEi3rpAK8mweQJyuq650aWorqA2p78ohq2MYoHH9PjrEkzQEG7w',
+                'outputFormat' => 'mp4',
+                'command'      => "-i {INPUTFILE} {OUTPUTFILE} -c:v libx264 -preset slow -crf 23 -profile:v baseline -level 3.0 -b:v 250k -maxrate 250k -bufsize 500k -movflags +faststart -pix_fmt yuv420p -c:a libfdk_aac -b:a 128k"
+            ]
+        ],
+    ]
+];
