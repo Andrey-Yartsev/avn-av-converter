4953a13215f0bc2b6aa06528469d1de6929ff37e
Alex Pavlov <salex.pel@gmail.com>
2018-11-14 02:02:07 +0300

    Disable

--- src/components/drivers/CloudConvertDriver.php
+++ src/components/drivers/CloudConvertDriver.php
@@ -198,7 +198,7 @@ class CloudConvertDriver extends Driver
         ]);
         $watermarkString = '';
         if (isset($watermark['text'])) {
-            $watermarkString = '-vf drawtext="fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:text=\'' . str_replace('/', "",$watermark['text']) . '\':x=w-tw-10:y=h-th-10:fontsize=20:fontcolor=Grey"';
+          //  $watermarkString = '-vf drawtext="fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:text=\'' . str_replace('/', "",$watermark['text']) . '\':x=w-tw-10:y=h-th-10:fontsize=20:fontcolor=Grey"';
         }
         $process->start([
             'outputformat'     => $this->outputFormat,
