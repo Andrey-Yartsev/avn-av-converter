afe786ee51cb0f861d961013933ec0f09f8bc9ac
Alex Pavlov <salex.pel@gmail.com>
2018-11-22 10:35:03 +0300

    Added amazon config

--- src/config/config.prod.of.php
+++ src/config/config.prod.of.php
@@ -29,5 +29,25 @@ return [
                 'command'      => "-i {INPUTFILE} {watermark} {OUTPUTFILE} -c:v libx264 -preset slow -crf 23 -profile:v baseline -level 3.0 -b:v 250k -maxrate 250k -bufsize 500k -movflags +faststart -pix_fmt yuv420p -c:a libfdk_aac -b:a 128k"
             ]
         ],
+        'ofamazon' => [
+            'video' => [
+                'driver' => \Converter\components\drivers\AmazonDriver::class,
+                'url'        => 'https://of2transcoder.s3.amazonaws.com',
+                's3'         => [
+                    'region' => 'us-east-1',
+                    'bucket' => 'of2transcoder',
+                    'key'    => 'AKIAIEGIIC3WZYGPIPCA',
+                    'secret' => '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'
+                ],
+                'transcoder' => [
+                    'region'   => 'us-east-1',
+                    'bucket'   => 'of2transcoder',
+                    'key'      => 'AKIAIEGIIC3WZYGPIPCA',
+                    'secret'   => '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',
+                    'pipeline' => '1542729803060-wvvyxu',
+                    'preset'   => '1351620000001-100070'
+                ]
+            ]
+        ],
     ]
 ];
