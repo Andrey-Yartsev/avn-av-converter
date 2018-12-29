bb97cff86fd18f1ebb04ce7f504e2ffb16512330
Pavel Gradoboyev <flashworking@gmail.com>
2018-12-29 22:50:20 +0300

    sRand for amazon upload

--- src/commands/AmazonUploadCommand.php
+++ src/commands/AmazonUploadCommand.php
@@ -18,45 +18,48 @@ use Symfony\Component\Console\Output\OutputInterface;
 class AmazonUploadCommand extends Command
 {
     use LockableTrait;
-    
+
     protected function configure()
     {
         $this->setName('amazon:upload');
     }
-    
+
     public function execute(InputInterface $input, OutputInterface $output)
     {
         if (!$this->lock()) {
             $output->writeln('Process already working!');
             return 1;
         }
-    
+
         $presents = Config::getInstance()->get('presets');
         while (true) {
-            $uploads = Redis::getInstance()->sMembers('amazon:upload');
-            foreach ($uploads as $upload) {
-                $params = json_decode($upload, true);
-                $output->writeln('<info>Catch ' . $upload . '</info>');
-                $presetName = $params['presetName'];
-                if (empty($presents[$presetName]) || empty($presents[$presetName]['video'])) {
-                    Redis::getInstance()->sRem('amazon:upload', $upload);
-                    continue;
-                }
-            
-                $output->writeln('<info>Init amazon driver</info>');
-                $amazonDriver = new AmazonDriver($presetName, $presents[$presetName]['video']);
-                if ($amazonDriver->createJob($params['filePath'], $params['callback'], $params['processId'], $params['watermark'])) {
-                    Redis::getInstance()->sRem('amazon:upload', $upload);
-                    $output->writeln('<info>Process #' . $params['processId'] . ' uploaded</info>');
-                } else {
-                    $output->writeln('<error>:(</error>');
+            $uploads = Redis::getInstance()->sRandMember('amazon:upload', 10);
+            if (empty($uploads)) {
+                sleep(1);
+            } else {
+                foreach ($uploads as $upload) {
+                    $params = json_decode($upload, true);
+                    $output->writeln('<info>Catch ' . $upload . '</info>');
+                    $presetName = $params['presetName'];
+                    if (empty($presents[$presetName]) || empty($presents[$presetName]['video'])) {
+                        Redis::getInstance()->sRem('amazon:upload', $upload);
+                        continue;
+                    }
+
+                    $output->writeln('<info>Init amazon driver</info>');
+                    $amazonDriver = new AmazonDriver($presetName, $presents[$presetName]['video']);
+                    if ($amazonDriver->createJob($params['filePath'], $params['callback'], $params['processId'], $params['watermark'])) {
+                        Redis::getInstance()->sRem('amazon:upload', $upload);
+                        $output->writeln('<info>Process #' . $params['processId'] . ' uploaded</info>');
+                    } else {
+                        $output->writeln('<error>:(</error>');
+                    }
                 }
             }
-            sleep(1);
         }
-        
+
         $this->release();
-    
+
         return 2;
     }
 }
