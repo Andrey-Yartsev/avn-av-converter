170f3506cd9d79fff6b873cc69b075944fcc53e4
Alex Pavlov <salex.pel@gmail.com>
2019-01-14 14:11:08 +0300

    Added worker status

--- src/controllers/SystemController.php
+++ src/controllers/SystemController.php
@@ -28,6 +28,7 @@ class SystemController extends Controller
             'amazon' => [
                 'queues' => count($redis->sMembers('amazon:queue')),
                 'upload' => count($redis->sMembers('amazon:upload')),
+                'workers' => exec('ps aux | grep worker:upload | wc -l')
             ],
             'cloudconvert' => [
                 'queues' => count($redis->keys('cc:*')),
