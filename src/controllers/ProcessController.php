0d7b29f6bba69502f614b1e7130d3e0afe065b74
Alex Pavlov <salex.pel@gmail.com>
2019-06-18 20:25:35 +0300

    Fixed CORS

--- src/controllers/ProcessController.php
+++ src/controllers/ProcessController.php
@@ -120,19 +120,6 @@ class ProcessController extends Controller
     
     public function actionUpload()
     {
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
-        header('Access-Control-Allow-Origin: ' . $origin);
-        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Expose-Headers: Range');
-        header('Access-Control-Allow-Headers: Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With,Content-Disposition,Content-Range,Content-Length,TE');
-        header('Allow: PUT, GET, DELETE, POST');
-    
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(204);
-            exit(0);
-        }
-        
         $request = $this->getRequest();
         $form = new UploadForm();
         if ($request->getContentType() == 'json') {
--- web/index.php
+++ web/index.php
@@ -14,13 +14,16 @@ define('PUBPATH', __DIR__);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$currentVerb = strtoupper($_SERVER["REQUEST_METHOD"]);
-if ($currentVerb == 'OPTIONS') {
-    http_response_code(200);
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
-    header('Allow: PUT, GET, DELETE, POST');
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
+header('Access-Control-Allow-Origin: ' . $origin);
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Expose-Headers: Range');
+header('Access-Control-Allow-Headers: Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With,Content-Disposition,Content-Range,Content-Length,TE');
+header('Allow: PUT, GET, DELETE, POST');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
     exit(0);
 }
 
