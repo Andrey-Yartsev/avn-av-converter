d1f00e06b3fe0b33ab112d0945946b0c6728d5ef
Alex Pavlov <salex.pel@gmail.com>
2019-07-09 23:33:22 +0300

    Fixed

--- src/components/drivers/LocalDriver.php
+++ src/components/drivers/LocalDriver.php
@@ -199,9 +199,9 @@ class LocalDriver extends Driver
             $width = $imageWidth > $width ? $width : $imageWidth;
             $height = $imageHeight > $height ? $height : $imageHeight;
             if ($imageWidth < $imageHeight) {
-                $height = ceil($imageHeight / ($imageWidth / $width));
-            } elseif ($imageWidth > $imageHeight) {
                 $width = ceil($imageWidth / ($imageHeight / $height));
+            } elseif ($imageWidth > $imageHeight) {
+                $height = ceil($imageHeight / ($imageWidth / $width));
             }
             $sizeBox = new Box($width, $height);
             $image->resize($sizeBox);
