dc2a8f0d5c9500c97e430e092663ac0dce1fcdad
Alex Pavlov <salex.pel@gmail.com>
2020-02-12 22:14:01 +0300

    Fixed check location

--- src/forms/UploadForm.php
+++ src/forms/UploadForm.php
@@ -51,7 +51,7 @@ class UploadForm extends Form
                     ]);
                     $this->file['ContentType'] = $response['ContentType'] ?? null;
                     $this->fileType = isset($response['ContentType']) ? FileHelper::getTypeByMimeType($response['ContentType']) : 'None';
-                    if (stripos($this->file['Location'], 'https://of2transcoder.s3-accelerate.amazonaws.com/upload/') !== 0) {
+                    if (!stripos($this->file['Location'], $this->file['Key'])) {
                         $this->setErrors('Invalid input.');
                         return false;
                     }
