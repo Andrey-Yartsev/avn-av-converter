730faa0623271e95e33c994404fc85d6b6914812
Alex Pavlov <salex.pel@gmail.com>
2020-04-03 16:00:38 +0300

    Fixed

--- src/config/config.prod.of2.php
+++ src/config/config.prod.of2.php
@@ -4,7 +4,6 @@ $cfg = require(__DIR__ . '/config.prod.of.php');
 
 $cfg['baseUrl'] = 'https://convert2.onlyfans.com';
 $cfg['presets']['of_beta']['video']['transcoder']['pipeline']
-    = $cfg['presets']['of_beta2']['video']['transcoder']['pipeline']
     = $cfg['presets']['of_geo_big']['video']['transcoder']['pipeline']
     = '1580329941216-vjf638';
 
