14c6c30de84bd798e93cf306fea74619f18b10ab
Alex Pavlov <salex.pel@gmail.com>
2020-04-08 10:00:58 +0300

    Fixed

--- console/index.php
+++ console/index.php
@@ -17,4 +17,5 @@ $application->add(new \Converter\commands\AmazonQueueCommand());
 $application->add(new \Converter\commands\RetryCommand());
 $application->add(new \Converter\commands\UploadCommand());
 $application->add(new \Converter\commands\FixCommand());
+$application->add(new \Converter\commands\FixSendCommand());
 $application->run();
