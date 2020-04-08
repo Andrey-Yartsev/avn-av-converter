88b566d411e026dee200cc5e1ad040eeff85c7a5
Alex Pavlov <salex.pel@gmail.com>
2020-04-08 13:16:16 +0300

    Configs for of3, of4

---
+++ src/config/config.prod.of4.php
@@ -0,0 +1,8 @@
+<?php
+
+$cfg = require(__DIR__ . '/config.prod.of.php');
+
+$cfg['baseUrl'] = 'https://convert4.onlyfans.com';
+$cfg['presets']['of_geo_big']['video']['transcoder']['pipeline'] = '1580329941216-vjf638';
+
+return $cfg;
